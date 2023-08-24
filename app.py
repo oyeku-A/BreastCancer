536c726886fde901ea0677ffdbc663aec412ae9d
oyeku-A <82169558+oyeku-A@users.noreply.github.com>
2023-08-24 15:30:52 +0000

    built landing page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home_page():
-  return "Work in progress"
+  return render_template('index.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
