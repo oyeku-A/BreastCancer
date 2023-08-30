a8e0473ca0775a40542af53dd88f2d03e9729557
oyeku-A <82169558+oyeku-A@users.noreply.github.com>
2023-08-30 07:57:35 +0000

    added reload button and now prediction displays on same page as form

--- app.py
+++ app.py
@@ -7,31 +7,32 @@ model = joblib.load("models/model.pkl")
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home_page():
-  return render_template('index.html')
+  form_data = {}
+  if request.method == 'POST':
+    clump_thickness = request.form['clump thickness']
+    uniformity_of_cell_size = request.form['uniformity of cell size']
+    uniformity_of_cell_shape = request.form['uniformity of cell shape']
+    marginal_adhesion = request.form['marginal adhesion']
+    single_epithelial_cell_size = request.form['single epithelial cell size']
+    bare_nuclei = request.form['bare nuclei']
+    bland_chromatin = request.form['bland chromatin']
+    normal_nucleoli = request.form['normal nucleoli']
+    mitoses = request.form['mitoses']
+    form_data = request.form.to_dict()
 
+    arr_inputs = np.array([[
+      clump_thickness, uniformity_of_cell_size, uniformity_of_cell_shape,
+      marginal_adhesion, single_epithelial_cell_size, bare_nuclei,
+      bland_chromatin, normal_nucleoli, mitoses
+    ]],
+                          dtype='int64')
 
-@app.route("/prediction", methods=['POST'])
-def prediction_page():
-  clump_thickness = request.form['clump thickness']
-  uniformity_of_cell_size = request.form['uniformity of cell size']
-  uniformity_of_cell_shape = request.form['uniformity of cell shape']
-  marginal_adhesion = request.form['marginal adhesion']
-  single_epithelial_cell_size = request.form['single epithelial cell size']
-  bare_nuclei = request.form['bare nuclei']
-  bland_chromatin = request.form['bland chromatin']
-  normal_nucleoli = request.form['normal nucleoli']
-  mitoses = request.form['mitoses']
-
-  arr_inputs = np.array([[
-    clump_thickness, uniformity_of_cell_size, uniformity_of_cell_shape,
-    marginal_adhesion, single_epithelial_cell_size, bare_nuclei,
-    bland_chromatin, normal_nucleoli, mitoses
-  ]], dtype='int64')
-
-  prediction = model.predict(arr_inputs)
-  return render_template('prediction.html', pred_=prediction)
+    prediction = model.predict(arr_inputs)
+    return render_template('index.html', pred_=prediction, form_data=form_data)
+  else:
+    return render_template('index.html', form_data=form_data)
 
 
 if __name__ == "__main__":
--- models/modelling.py
+++ models/modelling.py
@@ -10,9 +10,14 @@ X = df.iloc[:, 1:-1].values
 y = df.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.20,
+                                                    random_state=0)
 
 from xgboost import XGBClassifier
+
 bst = XGBClassifier()
 bst.fit(X_train, y_train)
 
